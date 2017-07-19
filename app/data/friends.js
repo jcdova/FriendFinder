c46652a7d2893d0164f92d24fa25aea7f0ddec11
jcdova <jcdova@gmail.com>
2017-07-19 12:22:34 -0700

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+var friendsArray = [
+    {
+      "name":"Angelina",
+      "photo":"http://popnewz.com/wp-content/uploads/2015/10/bigstock-LOS-ANGELES-DEC-Angelina-40445332.jpg",
+      "scores":[
+          5,
+          1,
+          4,
+          4,
+          5,
+          1,
+          2,
+          5,
+          4,
+          1
+      ]
+    },
+    {
+      "name":"Selma",
+      "photo":"http://jonvilma.com/images/salma-hayek-1.jpg",
+      "scores":[
+          4,
+          5,
+          3,
+          2,
+          1,
+          1,
+          3,
+          5,
+          4,
+          2
+      ]
+    }
+  ];
+
+  module.exports = friendsArray;
